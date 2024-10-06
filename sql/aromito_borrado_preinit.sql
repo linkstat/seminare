ff74f729c3a967a9339e12cae00d8fdef1921798
linkstat <linkstat@gmail.com>
2024-10-06 04:45:53 -0300

    Terminadas consultas SQL de creacion de tablas

--- sql/aromito_borrado_preinit.sql
+++ sql/aromito_borrado_preinit.sql
@@ -1,6 +1,10 @@
 -- Borrado total de las tablas de la BD
 USE aromito;
 
+-- Borrado de las funciones personalizadas
+DROP FUNCTION IF EXISTS UUID_TO_BIN;
+DROP FUNCTION IF EXISTS BIN_TO_UUID2;
+
 -- Desactiva la verificación de claves foráneas
 SET FOREIGN_KEY_CHECKS = 0;
 
