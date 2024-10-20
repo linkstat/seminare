2c0435b55c1fa56d3aab942287518398b52aae38
Pablo Alejandro Hamann <linkstat@gmail.com>
2024-10-20 11:46:35 -0300

    Separación de capas. Movida las tareas de BD a clases especificas. Cambios en la interfaz grafica de pantalla de login: autoformatea el ingreso de CUIL

--- src/main/java/ar/com/hmu/repository/DatabaseConnector.java
+++ src/main/java/ar/com/hmu/repository/DatabaseConnector.java
@@ -7,6 +7,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Clase responsable de gestionar la conexión a la base de datos.
+ *
+ * La clase `DatabaseConnector` utiliza los valores de configuración proporcionados por {@link ConfigReader}
+ * para establecer una conexión con la base de datos especificada. Actualmente, soporta la conexión
+ * a bases de datos de tipo MariaDB (y desestima otros motores de BD, indicando que no están soportados).
+ */
 public class DatabaseConnector {
 
     private final String dbType;
@@ -16,6 +23,14 @@ public class DatabaseConnector {
     private final String username;
     private final String password;
 
+    /**
+     * Constructor que inicializa el conector de base de datos utilizando un objeto {@link ConfigReader}.
+     *
+     * Este constructor recupera la configuración de la base de datos a través de un {@link ConfigReader},
+     * que lee los detalles de conexión desde un archivo de configuración (por ejemplo, YAML).
+     *
+     * @param configReader una instancia de {@link ConfigReader} que proporciona los valores de configuración necesarios.
+     */
     public DatabaseConnector(ConfigReader configReader) {
         // Obtenemos la configuración desde ConfigReader
         DatabaseConfig dbConfig = configReader.getDbConfig();
@@ -29,6 +44,16 @@ public class DatabaseConnector {
         this.password = dbConfig.getPassword();
     }
 
+    /**
+     * Establece y devuelve una conexión con la base de datos.
+     *
+     * Este método crea una conexión a la base de datos utilizando los valores de configuración previamente
+     * obtenidos. Actualmente, solo se soporta la conexión a bases de datos de tipo MariaDB.
+     *
+     * @return una instancia de {@link Connection} que representa la conexión activa a la base de datos.
+     * @throws SQLException si ocurre un error al intentar conectar a la base de datos.
+     * @throws UnsupportedOperationException si el tipo de base de datos no es soportado.
+     */
     public Connection getConnection() throws SQLException {
         if (!dbType.equalsIgnoreCase("mariadb")) {
             throw new UnsupportedOperationException("Motor de base de datos no implementado: " + dbType);
