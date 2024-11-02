ae222b1bb35c630671198b766f9bb7b6137445fb
Pablo Alejandro Hamann <linkstat@gmail.com>
2024-11-02 02:52:24 -0300

    Deshabilitar menu contextual en elemento WebView del modal Acerca de

--- src/main/java/ar/com/hmu/utils/AppInfo.java
+++ src/main/java/ar/com/hmu/utils/AppInfo.java
@@ -5,26 +5,12 @@ public class AppInfo {
     public static final String VERSION = "1.0.0";
     public static final String BUILD = "110123";
     public static final String PRODUCTION_DATE = "Nov. 2024";
-    /*public static final String PRODUCT_INFO = "<html>"
-            + "<body>"
-            + "<h1>Información del Producto</h1>"
-            + "<p>Esta es una <b>aplicación</b> para gestionar ausentismo hospitalario.</p>"
-            + "<p>Puedes utilizar <i>negritas</i>, <u>subrayado</u> y otros formatos básicos.</p>"
-            + "</body>"
-            + "</html>";
-     */
-    /*public static final String PRODUCT_INFO = "Este es un producto de desarrollo de software para el " +
-            "Seminario de Práctica Profesional de Lic. en Informática de la UES21." +
-            "\n" +
-            "\nPersonas involucradas en el proyecto:\n" +
-            "\n-------------------------------------" +
-            "\nPablo Alejandro Hamann (alumno responsable del desarrollo)" +
-            "\nIng. Pablo Alejandro Virgolini (docente tutor a cargo de la supervisión y seguimiento)" +
-            "\n" +
-            "\n<i>Copyleft</i> 2024 - desarrollado bajo licencia GPLv3" +
-            "\n";
-    */
     public static final String PRODUCT_INFO = "<html>"
+            + "<head>"
+            + "    <script>"
+            + "        document.addEventListener('contextmenu', function(e) { e.preventDefault(); });"
+            + "    </script>"
+            + "</head>"
             + "<h3>Informaci&oacute;n del Producto</h3>"
             + "<div>"
             + "    <blockquote>"
@@ -53,6 +39,5 @@ public class AppInfo {
             + "<p>&nbsp;</p>"
             + "<div style=\"text-align: right;\"><em>Copyleft&nbsp;&copy; </em>2024 - desarrollado bajo licencia <a href=\"https://www.gnu.org/licenses/lgpl-3.0.html#license-text\">LGPLv3. </a></div>"
             + "</html>";
-
 
 }
